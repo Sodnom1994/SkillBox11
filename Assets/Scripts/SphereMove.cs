65e7d1e11dcc8a891c372edb2bae4775b5cfe447
Sodnom1994 <93848263+Sodnom1994@users.noreply.github.com>
2025-02-09 11:29:03 +0300

    5

---
+++ Assets/Scripts/SphereMove.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMove : MonoBehaviour
+{
+    private Rigidbody _sphereRB;
+    private Transform _transformStart;
+    private Animation _sphereAnimation;
+    private void Start()
+    {
+        _transformStart = _sphereRB.transform;
+    }
+
+    private void Update()
+    {
+        if(_sphereRB.position != _transformStart.position)
+        {
+            
+        }
+    }
+}
