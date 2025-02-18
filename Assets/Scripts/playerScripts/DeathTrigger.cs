2b253f22c9749f4da34e18d25159c7a6a798375f
Sodnom1994 <93848263+Sodnom1994@users.noreply.github.com>
2025-02-18 23:25:16 +0300

    8.5

---
+++ Assets/Scripts/playerScripts/DeathTrigger.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTrigger : MonoBehaviour
+{
+    public ParticleSystem deathAnimation;
+    private void OnDestroy()
+    {
+        deathAnimation.Play();
+    }
+}
