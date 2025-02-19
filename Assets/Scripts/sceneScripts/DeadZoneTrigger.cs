bf2b0bd4e08765a6fdc83447431fe4de53d4a834
Sodnom1994 <93848263+Sodnom1994@users.noreply.github.com>
2025-02-19 12:51:57 +0300

    8.9

--- Assets/Scripts/sceneScripts/DeadZoneTrigger.cs
+++ Assets/Scripts/sceneScripts/DeadZoneTrigger.cs
@@ -7,7 +7,15 @@ using UnityEngine.UIElements;
 public class DeadZoneTrigger : MonoBehaviour
 {
     public GameObject gameOverPanel;
-    
+    public ParticleSystem deathParticle;
+
+    private void Start()
+    {
+        if(gameOverPanel == null || deathParticle ==null)
+        {
+            Debug.LogError("Проверь компонент у объекта");
+        }    
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -16,7 +24,7 @@ public class DeadZoneTrigger : MonoBehaviour
         }
         IEnumerator GameOver(Collider other)
         {
-            
+            deathParticle.Play();
             Destroy(other.gameObject);
             yield return new WaitForSeconds(2);
             gameOverPanel.SetActive(true);
